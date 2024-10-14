cf1915ea0483065f133617a56bc5324e2b93a425
Userphenixe <hamabbas910@gmail.com>
2024-10-14 21:47:01 +0100

    Configuring Orders And Customers Views

--- Smart_Inventory_Management_API/Inventory/myapp/urls.py
+++ Smart_Inventory_Management_API/Inventory/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home, ProductsView, ProductViewItem, SuppliersView, SupplierViewItem, CategoriesView, CategoryViewItem
-
+from .views import home, ProductsView, ProductViewItem, SuppliersView, SupplierViewItem, CategoriesView, CategoryViewItem, CustomerListView, OrderListView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', home, name='home'),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('suppliers/<int:pk>', SupplierViewItem.as_view(), name='SupplierViewItem'),
     path('categories/', CategoriesView.as_view(), name='CategoriesView'),
     path('categories/<int:pk>', CategoryViewItem.as_view(), name='CategoryViewItem'),
+    path('customers/', CustomerListView.as_view(), name= 'CustomerListView'),
+    path('orders/', OrderListView.as_view(), name='OrderListView'),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- Smart_Inventory_Management_API/Inventory/myapp/views.py
+++ Smart_Inventory_Management_API/Inventory/myapp/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .serializers import SerializerProduct, SerializerSupplier, SerializerCategory
+from .serializers import SerializerProduct, SerializerSupplier, SerializerCategory, SerializerCustomer, SerializerOrder
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from .models import Product, Supplier, Category
+from .forms import FormCustomer, FormOrder
+from .models import Product, Supplier, Category, Customer, Order
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from datetime import datetime
 
 def home(request):
     return HttpResponse('Welcome!')
+
+class CustomerListView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        date = request.GET.get('date', datetime.today().date())
+        customers = Customer.objects.all()
+        serializer = SerializerCustomer(customers, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = SerializerCustomer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class OrderListView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        date = request.GET.get('date', datetime.today().date())
+        orders = Order.objects.all()
+        serializer = SerializerOrder(orders, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer =SerializerOrder(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductsView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
