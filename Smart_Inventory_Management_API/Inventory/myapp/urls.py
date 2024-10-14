b36fb73bed908b9d4fcfda17b7c1e85aa25bef5e
Userphenixe <hamabbas910@gmail.com>
2024-10-14 20:45:46 +0100

    Configuring Suppliers Views

--- Smart_Inventory_Management_API/Inventory/myapp/urls.py
+++ Smart_Inventory_Management_API/Inventory/myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, ProductsView, ProductViewItem
+from .views import home, ProductsView, ProductViewItem, SuppliersView, SupplierViewItem
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('menu/', ProductsView.as_view(), name= 'ProductsView'),
     path('menu/<int:pk>', ProductViewItem.as_view(), name= 'ProductViewItem'),
+    path('suppliers/', SuppliersView.as_view(), name='SuppliersView'),
+    path('suppliers/<int:pk>', SupplierViewItem.as_view(), name='SupplierViewItem'),
 ]
--- Smart_Inventory_Management_API/Inventory/myapp/views.py
+++ Smart_Inventory_Management_API/Inventory/myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .serializers import SerializerProduct
+from .serializers import SerializerProduct, SerializerSupplier
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
-from .models import Product
+from .models import Product, Supplier
 
 def home(request):
     return HttpResponse('Welcome!')
@@ -13,7 +13,17 @@ class ProductsView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = SerializerProduct
 
-class ProductViewItem(generics.RetrieveAPIView):
+class ProductViewItem(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = SerializerProduct
+
+class SuppliersView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Supplier.objects.all()
+    serializer_class = SerializerSupplier
+
+class SupplierViewItem(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Supplier.objects.all()
+    serializer_class = SerializerSupplier
