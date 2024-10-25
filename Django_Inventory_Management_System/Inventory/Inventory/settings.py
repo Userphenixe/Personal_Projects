7e13bbabdda42555ac1fe0f9d3ceedfb98a8150c
Userphenixe <hamabbas910@gmail.com>
2024-10-25 13:55:27 +0100

    Config User Registration

--- Django_Inventory_Management_System/Inventory/Inventory/settings.py
+++ Django_Inventory_Management_System/Inventory/Inventory/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Dashboards',
     'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
